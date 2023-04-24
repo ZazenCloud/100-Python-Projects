da2e74e6bb47fa94e8fc159bce04b9e3a95f75df
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-04-24 19:17:30 -0300

    Add files via upload

---
+++ Projects/Day 03 - Desert Island.py
@@ -0,0 +1,227 @@
+print(r'''
+                                                   .       .
+                                                    \     /
+                                                 ._  '   '  _.
+                                                   '  o@o  '
+                                                     o@@@o
+                                                 .-'  o@o  '-.
+                                                     .   .
+                                                    /     \
+                                                   .       .
+
+                             'Xx  xX*,
+                          ,*xXXx_xXx
+                            _xXXXXXxx*,
+                          ,*XXx@x@Xx
+                            X @|@@ `x
+                            '  ||    '
+                               ||
+                               ||
+                               ||
+                               ||
+                            /ssssssss.
+                      /sssssssSSSSssssssssss.
+                 /sssssSSSSSSSSSSSSSSSssssssssssss.              
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+''')
+
+hallucination = r'''
+You are hallucinating!
+
+             ________
+        _jgN########Ngg_
+      _N##N@@""  ""9NN##Np_
+     d###P            N####p
+     "^^"              T####
+                       d###P
+                    _g###@F
+                 _gN##@P
+               gN###F"
+              d###F
+             0###F
+             0###F
+             0###F
+             "NN@'
+
+              ___
+             q###r
+
+
+        G A M E  O V E R
+'''     
+monkey = r'''
+A monkey sucker punches you into a coma.
+
+                      __,__
+             .--.  .-"     "-.  .--.
+            / .. \/  .-. .-.  \/ .. \
+           | |  '|  /   Y   \  |'  | |
+           | \   \  \ 0 | 0 /  /   / |
+            \ '- ,\.-"`` ``"-./, -' /
+             `'-' /_   ^ ^   _\ '-'`
+             .--'|  \._ _ _./  |'--.
+           /`    \   \.-.  /   /    `\
+          /       '._/  |-' _.'       \
+         /          ;  /--~'   |       \
+        /        .'\|.-\--.     \       \
+       /   .'-. /.-.;\  |\|'~'-.|\       \
+       \       `-./`|_\_/ `     `\'.      \
+        '.      ;     ___)        '.`;    /
+          '-.,_ ;     ___)          \/   /
+           \   ``'------'\       \   `  /
+            '.    \       '.      |   ;/_
+          ___>     '.       \_ _ _/   ,  '--.
+        .'   '.   .-~~~~~-. /     |--'`~~-.  \
+       // / .---'/  .-~~-._/ / / /---..__.'  /
+      ((_(_/    /  /      (_(_(_(---.__    .'
+                | |     _              `~~`
+                | |     \'.
+                 \ '....' |
+                  '.,___.'
+
+
+               G A M E  O V E R
+'''
+
+time = r'''
+You wait. Time passes. Again. And again.
+You are older now.
+The river is the same.
+You realize that there is nothing here with more value than time.
+Pondering, you find a boat and row away from the island.
+
+        _____
+     _.'_____`._
+   .'.-'  12 `-.`.
+  /,' 11      1 `.\
+ // 10      /   2 \\
+;;         /       ::
+|| 9  ----O      3 ||
+::                 ;;
+ \\ 8           4 //
+  \`. 7       5 ,'/
+   '.`-.__6__.-'.'
+    ((-._____.-))
+    _))       ((_
+   '--'       '--'
+
+  G A M E   O V E R   
+'''
+
+
+red = r'''
+Cautiously, you enter through the red glowing opening.
+The minimalistic design and the metal walls confuses you.
+You soon conclude that you are in a surgery room.
+Scared, you lock your eyes with an alien being.
+Your mind is flooded with "You are next!".
+
+              .-""""-.
+             /        \
+            /_        _\
+           // \      / \\
+           |\__\    /__/|
+            \    ||    /
+             \        /
+              \  __  /  \  /         
+               '.__.'    \/          
+                |  |     /\         
+                |  |    O  O        
+                ----    //         
+               (    )  //        
+              (\\     //       
+             (  \\    )      
+             (   \\   )   /\
+   ___[\______/^^^^^^^\__/) o-)__
+  |\__[=======______//________)__\
+  \|_______________//____________|
+      |||      || //||     |||
+      |||      || @.||     |||
+       ||      \/  .\/      ||
+                  . .
+                 '.'.`
+                 
+          G A M E   O V E R       
+'''
+
+green = r'''
+Cautiously, you enter through the green glowing opening.
+An extraterrestrial being materializes right in front of you.
+"How dare you enter my spacecraft with your shoes on?" - he screams, telepathically.
+You fell your body becoming lighter, as particles flow through the air.
+In the same way it appeared, you are disappearing.
+
+.     .       .  .   . .   .   . .    +  .
+  .     .  :     .    .. :. .___---------___.
+       .  .   .    .  :.:. _".^ .^ ^.  '.. :"-_. .
+    .  :       .  .  .:../:            . .^  :.:\.
+        .   . :: +. :.:/: .   .    .        . . .:\
+ .  :    .     . _ :::/:               .  ^ .  . .:\
+  .. . .   . - : :.:./.                        .  .:\
+  .      .     . :..|:                    .  .  ^. .:|
+    .       . : : ..||        .                . . !:|
+  .     . . . ::. ::\(                           . :)/
+ .   .     : . : .:.|. ######              .#######::|
+  :.. .  :-  : .:  ::|.#######           ..########:|
+ .  .  .  ..  .  .. :\ ########          :######## :/
+  .        .+ :: : -.:\ ########       . ########.:/
+    .  .+   . . . . :.:\. #######       #######..:/
+      :: . . . . ::.:..:.\           .   .   ..:/
+   .   .   .  .. :  -::::.\.       | |     . .:/
+      .  :  .  .  .-:.":.::.\             ..:/
+ .      -.   . . . .: .:::.:.\.           .:/
+.   .   .  :      : ....::_:..:\   ___  :/
+   .   .  .   .:. .. .  .: :.:.:\       :/
+     +   .   .   : . ::. :.:. .:.|\  .:/|
+     .         +   .  .  ...:: ..|  --.:|
+.      . . .   .  .  . ... :..:.."(  ..)"
+ .   .       .      :  .   .: ::/  .  .::\
+     
+             G A M E   O V E R 
+'''
+
+blue = r'''
+Cautiously, you enter through the blue glowing opening.
+"It is... a trophy room?", you think, staring at shelves full of gold trophies and holographic photos.
+It seems this spaceship belongs to the best space soccer team of the universe.
+A split second goes by.
+You grab one and start to run.
+Euphoria rushes through your blood as you imagine the resale value of alien gold.
+But then you remember, you are still on a desert island.
+
+             ___________
+            '._==_==_=_.'
+            .-\:      /-.
+           | (|:.     |) |
+            '-|:.     |-'
+              \::.    /
+               '::. .'
+                 ) (
+               _.' '._
+              `"""""""`
+              YOU WIN!
+'''
+
+
+first_input = input("""You wake up disoriented in a desert island. \nAhead of you, there is a dense jungle. \nYou think about entering it or following through the beach. \n\nType 'jungle' or 'beach' - """).lower()
+if first_input == 'beach':
+    second_input = input("""\nAs you walk through the beach, you catch sight of a large river that empties into the ocean. \nOn the other side of it there is a strange object. \n\nType 'swim' or 'wait' - """).lower()
+    if second_input == 'swim':
+        third_input = input("""\nYou swim across the river and get to the other side. \nAs you approach the strange object, you realize it is actually an alien spacecraft. \nThere are 3 entrances, each glowing in a different color. \nYou think of going back, but your curiosity gets the best of you.\n\nType 'red', 'green' or 'blue' - """).lower()
+        if third_input == 'red':
+            print(red)
+        elif third_input == 'green':
+            print(green)
+        elif third_input == 'blue':
+            print(blue)
+        else:
+            print(hallucination)
+    elif second_input == 'wait':
+        print(time)
+    else:
+        print(hallucination)
+elif first_input == 'jungle':
+    print(monkey)
+else:
+    print(hallucination)
