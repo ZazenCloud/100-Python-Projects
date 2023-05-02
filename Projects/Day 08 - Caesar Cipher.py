bbacdda03d0a072777d58b6aa3be209899267607
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-05-02 16:27:55 -0300

    Update Day 08 - Caesar Cipher.py

--- Projects/Day 08 - Caesar Cipher.py
+++ Projects/Day 08 - Caesar Cipher.py
@@ -1,5 +1,25 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+option = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+plain_text = input("Type your message:\n").lower()
+shift_amount = int(input("Type the shift number:\n"))
+
+def caesar(text, shift, direction):
+  cipher_text = ''
+  if direction == 'decode':
+      shift *= -1
+  for letter in text:
+    position = alphabet.index(letter)
+    new_position = position + shift
+    if new_position > 25:
+      new_position = new_position % 26
+    elif new_position < 0:
+      new_position = new_position % -26
+    cipher_text += alphabet[new_position]
+
+  if direction == 'encode' or direction == 'decode':
+    print(f'The {direction}d text is {cipher_text}')
+  else:
+    print('Choose the encode or decode option.')
+
+caesar(text=plain_text, shift=shift_amount, direction=option)
