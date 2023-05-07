515541afd7af823a59d7073a4dc6c6a4e114a442
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-05-07 11:46:01 -0300

    Update Day 11 - BlackJack.py

--- Projects/Day 11 - BlackJack.py
+++ Projects/Day 11 - BlackJack.py
@@ -18,7 +18,15 @@ def clear():
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
+    '''Returns a random card from the deck.'''
     return random.choice(cards)
+
+def calculate_score(cards):
+    '''Take a list of cards and return the score calculated from the cards.'''
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
 
 keep_playing = True
 
@@ -26,10 +34,11 @@ while keep_playing:
     play_blackjack = input("Do you want to play a game of Blackjack. Type 'y' or 'n': ").lower()
     if play_blackjack == 'n':
         keep_playing = False
+        break
     clear()
     print(logo)
     player_cards = [deal_card(), deal_card()] # Adds two cards to the player's hand
-    player_score = sum(player_cards) # Current score
+    player_score = calculate_score(player_cards) # Current score
     dealer_cards = [deal_card()] # Adds one card to the dealer's had
     
     next_card = True
@@ -42,28 +51,28 @@ while keep_playing:
             next_card = False
             break
         player_cards += [deal_card()]
-        player_score = sum(player_cards)
-        if 11 in player_cards and player_score > 21:
-            player_score -= 10
+        player_score = calculate_score(player_cards)
     
-    dealer_score = sum(dealer_cards) # Current dealer score
+    dealer_score = calculate_score(dealer_cards) # Current dealer score
     
     while dealer_score < 17:
-        dealer_cards += [deal_card()] # Dealer draws cards until a score of > 16 OR > players_score
-        dealer_score = sum(dealer_cards)
-        if 11 in dealer_cards and dealer_score > 21:
-            dealer_score -= 10
+        dealer_cards += [deal_card()] # Dealer draws cards until a score of > 16
+        dealer_score = calculate_score(dealer_cards)
     
     print(f'Your final hand: {player_cards}. Final score: {player_score}.')
     print(f"Dealer's final hand: {dealer_cards}. Final score: {dealer_score}.")
     
+    if player_score == dealer_score:
+        print('\nDraw!')    
+    if 11 in player_cards and 10 in player_cards:
+        print('\nYou win with a Blackjack!')
+    if 11 in dealer_cards and 10 in dealer_cards:
+        print('\nDealer got a Blackjack. You lose!') 
     if player_score > 21:
-        print('You went over. You lose!')
+        print('\nYou went over. You lose!')
     elif dealer_score > 21:
-        print('Dealer went over. You win!')        
-    elif player_score == dealer_score:
-        print('Draw!')        
+        print('\nDealer went over. You win!')        
     elif player_score > dealer_score:
-        print('You win!')
+        print('\nYou win!')
     elif player_score < dealer_score:
-        print('You lose!')
+        print('\nYou lose!')
