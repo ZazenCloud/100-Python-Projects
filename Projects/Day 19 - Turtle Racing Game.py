d507130c4c8a8536d5127321347603003d09a5f9
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-05-18 21:30:40 -0300

    Update Day 19 - Turtle Racing Game.py

--- Projects/Day 19 - Turtle Racing Game.py
+++ Projects/Day 19 - Turtle Racing Game.py
@@ -1,26 +1,40 @@
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? \nEnter a color: ")
+user_bet = screen.textinput(
+  title="Make your bet!",
+  prompt=
+  "Which turtle will win the race? \n\nChoose a color:\nBlack - Red - Green - Blue"
+)
 print(user_bet)
-colors = ["black", "red", "green", "blue"]
-    
+colors = ["blue", "green", "red", "black"]
+y_position = [-100, -35, 35, 100]
+all_turtles = []
+game_running = False
 
-black_turtle = Turtle(shape="turtle")
-black_turtle.penup()
-black_turtle.goto(x=-230, y=90)
+for turtle_index in range(4):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
 
-red_turtle = Turtle(shape="turtle")
-red_turtle.penup()
-red_turtle.goto(x=-230, y=30)
+if user_bet:
+    game_running = True
 
-green_turtle = Turtle(shape="turtle")
-green_turtle.penup()
-green_turtle.goto(x=-230, y=-30)
+while game_running:
+    for turtle in all_turtles:
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
-blue_turtle = Turtle(shape="turtle")
-blue_turtle.penup()
-blue_turtle.goto(x=-230, y=-90)
+        if turtle.xcor() > 235:
+            game_running = False
+            winning_turtle = turtle.pencolor()
+            if winning_turtle == user_bet.lower():
+                print(f"You've won! The {winning_turtle} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_turtle} turtle is the winner!")
 
 screen.exitonclick()
