ba17e7dd6277d2b06d75ccdadd21a4f91be2a4bb
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-05-23 16:07:30 -0300

    Add files via upload

---
+++ Projects/Day 23 - Turtle Crossing Game.py
@@ -0,0 +1,44 @@
+import time
+from turtle import Screen
+from player import Player, Scoreboard, CarManager
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+# Turn off the automatic screen updates
+scoreboard.score()
+
+screen.listen()
+screen.onkeypress(player.move_up, "Up")
+screen.onkeypress(player.move_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    # Create a new car
+    car_manager.create_car()
+    # Move all the cars
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        # Reset the player's position
+        player.go_to_start()
+        # Increase the car speed
+        car_manager.level_up()
+        # Update the score
+        scoreboard.score()
+
+screen.exitonclick()
---
+++ Projects/turtle_crossing.py
@@ -0,0 +1,90 @@
+from turtle import Turtle
+import random
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__("turtle")
+        self.penup()
+        self.setheading(90)
+        self.go_to_start()
+
+    def go_to_start(self):
+        # Move the player to the starting position
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def move_down(self):
+        self.backward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        # Check if the player has reached the finish line
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-200, 250)
+        self.current_level = 0
+
+    def score(self):
+        # Update the scoreboard with the current level
+        self.clear()
+        self.current_level += 1    
+        self.write(f"Level: {self.current_level}", align="center", font=FONT)
+    
+    def game_over(self):
+        # Display the game over message
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
+
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 5
+
+
+class CarManager(Turtle):
+
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        # Randomly create a car (1 in 6 chance)
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.penup()
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        # Move all the cars from right to left
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        # Increase the car speed for the next level
+        self.car_speed += MOVE_INCREMENT
