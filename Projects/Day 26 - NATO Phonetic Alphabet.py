3c1531d843b2360373d1b008d2719273d09a7c37
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-05-31 20:58:27 -0300

    Add files via upload

---
+++ Projects/Day 26 - NATO Phonetic Alphabet.py
@@ -0,0 +1,11 @@
+import pandas
+# Download nato_phonetic_alphabet.csv (located on this repository) and move it to same folder of this file
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+# Create a dictionary mapping each letter to its corresponding NATO code
+nato_dictionary = {row.letter: row.code for index, row in data.iterrows()}
+
+# Convert each letter of the word to its corresponding NATO phonetic code
+word = input("Enter a word: ").upper()
+phonetic_word = [nato_dictionary[letter] for letter in word]
+print(phonetic_word)
