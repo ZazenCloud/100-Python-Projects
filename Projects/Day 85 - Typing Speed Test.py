0946a3afee3b4aaa3dbc0fb52c251fd7047c09d3
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-08-18 23:23:18 -0300

    Add files via upload

---
+++ Projects/Day 85 - Typing Speed Test.py
@@ -0,0 +1,28 @@
+import random
+
+
+
+
+# randomize list of 250 words
+
+# start button
+
+# clock
+# focus on text
+
+# every word (space), calculates score
+# only counts if word is correct
+
+# space goes back to the next word
+# if no chars, just add a space
+
+# press backspace delete chars
+# if no chars, go back to the last word
+
+
+
+
+# CPM
+# WPM
+# Time left
+# High score
