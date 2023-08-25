67bf23cc1fdd9972ef8bb42bc6ff102266ce896f
ZazenCloud <cloudzazen@gmail.com>
2023-08-25 18:28:55 -0300

    WIP: Implement Word Highlighting and Word Checking

--- Projects/Day 85 - Typing Speed Test/Day 85 - Typing Speed Test.py
+++ Projects/Day 85 - Typing Speed Test/Day 85 - Typing Speed Test.py
@@ -22,20 +22,29 @@ start_button = ''
 
 entry_box = ''
 
+random_words = random.choices(words.word_list, k=300)
+
+# Divide the 300 random words in 160 lists of 5 words
+num_lines = 60
+words_per_line = 5
+lines_list = [
+    random_words[i * words_per_line:(i + 1) * words_per_line] for i in range(num_lines)
+]
+
 
 def start_countdown():
     global countdown_started
-    if not countdown_started:
-        # Set the flag to True
-        countdown_started = True
-        # Get the current time in seconds
-        current_time = time.time()
-        # Set the countdown duration to 1 minute
-        duration = 60
-        # Calculate the end time
-        end_time = current_time + duration
-        # Create a thread to update the label with the remaining time
-        threading.Thread(target=update_label, args=(end_time,)).start()
+
+    # Set the flag to True
+    countdown_started = True
+    # Get the current time in seconds
+    current_time = time.time()
+    # Set the countdown duration to 1 minute
+    duration = 60
+    # Calculate the end time
+    end_time = current_time + duration
+    # Create a thread to update the label with the remaining time
+    threading.Thread(target=update_label, args=(end_time,)).start()
 
 
 def update_label(end_time):
@@ -52,6 +61,18 @@ def update_label(end_time):
         time.sleep(1)
         update_label(end_time)
 
+        
+def find_and_change(word):
+    # remove any previous tags
+    text_box.tag_remove('green', '1.0', 'end')
+    # search for the word from the beginning of the text
+    index = text_box.search(word, '1.0', 'end')
+    print(index)
+    # if found, apply the tag to the word
+    if index:
+        end_index = f'{index}+{len(word)}c'
+        text_box.tag_add('green', index, end_index)
+
 
 def start_test():
     pass
@@ -59,42 +80,89 @@ def start_test():
 
 def stop_test():
     pass
+counta = 0
 
+word_index = 0
+line_index = 0
+written_words = []
+current_word = lines_list[0][0]
+last_word = ''
 
 def space_action():
-    print("SPACE")
+    global word_index
+    global line_index
+    global current_word
 
-mark = 0
+    new_word = entry_text.get().lower().strip()
+    entry_text.delete(0, END)
+    written_words.append(new_word)
+    word_index += 1
+    if word_index > 4:
+        line_index += 1
+        word_index = 0
+        refresh_text_box()
+    current_word = lines_list[line_index][word_index]
+    if word_index > 0:
+        last_word = lines_list[line_index][word_index - 1]
+    else:
+        last_word = lines_list[line_index - 1][4]
+    find_and_change(current_word)
+    index = text_box.search(last_word, '1.0', 'end')
+    end_index = f'{index}+{len(last_word)}c'
+    text_box.tag_remove('white', '1.0', 'end')
+    if new_word == last_word:
+        text_box.tag_add('blue', index, end_index)
+    else:
+        text_box.tag_add('red', index, end_index)
+    print(current_word)
+    print(written_words)
+
 
 def backspace_action():
-    global mark
-    mark += 1
+    global word_index
+    global line_index
+    global current_word
+
+    word_index -= 1
+    if word_index < 0 and line_index > 0:
+        line_index -= 1
+        word_index = 4
+        refresh_text_box()
+    current_word = lines_list[line_index][word_index]
+    old_word = written_words.pop(word_index + (5 * line_index))
+    find_and_change(current_word)
+    entry_text.delete(0, END)
+    entry_text.insert(0, old_word)
+
+
+# def calculate_word_position():
+#     pass
+
+# def find_word():
+#     pass
+
+
+def refresh_text_box():
     text_box.delete(1.0, END)
-    text_box.insert(INSERT, "\n".join([" ".join(item) for item in lines_list[mark:mark+2]])) # insert the first 2 items of the big list
+    text_box.insert(INSERT, "\n".join([" ".join(item) for item in lines_list[line_index:line_index+2]]))
     text_box.delete("1.0 + 2 lines", END)
-    print(mark)
 
 
-# randomize list of 250 words
+def compare_letters(current_word):
+    word_index = text_box.search(current_word, '1.0', 'end')
+    captured_text = entry_text.get()
+    letter_index = int(word_index.split(".")[-1]) + len(captured_text) - 1
+    last_letter = captured_text[-1]
+    if last_letter == current_word[len(captured_text) - 1]:
+        text_box.tag_add('white', f'1.{letter_index}')
+    else:
+        text_box.tag_add('red', f'1.{letter_index}')
 
 # every word (space), calculates score
 # only counts if word is correct
 
-# space goes back to the next word
-# if no chars, just add a space
+# print(lines_list)
 
-# press backspace delete chars
-# if no chars, go back to the last word
-
-ops = random.choices(words.word_list, k=800)
-
-# Divide the 800 random words in 160 lists of 5 words
-num_lines = 160
-words_per_line = 5
-lines_list = [
-    ops[i * words_per_line:(i + 1) * words_per_line] for i in range(num_lines)
-]
-print(lines_list)
 
 root = Tk()
 root.title('Typing Speed Test')
@@ -104,33 +172,26 @@ time_left_label = Label(text='Time left: 60')
 time_left_label.pack()
 
 text_box = Text(wrap=WORD)
-text_box.insert(INSERT, "\n".join([" ".join(item) for item in lines_list[mark:mark+2]]))
-text_box.delete("1.0 + 2 lines", END) 
-# text_box.insert(END, ops)
+refresh_text_box()
 text_box.pack()
 
 entry_text = Entry()
 entry_text.focus()
 entry_text.pack()
 
-# text_box.tag_config('green', background='green')
-# text_box.tag_add('green', StringVar)
+text_box.tag_config('green', background='green')
+text_box.tag_config('red', foreground='red')
+text_box.tag_config('white', foreground='white')
+text_box.tag_config('blue', foreground='blue')
 
-root.bind("<Key>", lambda e: backspace_action() if e.char.isalpha() else None)
+root.bind("<Key>", lambda e: start_countdown() if e.char.isalpha() and not countdown_started else None)
 
-# root.bind("<Space>", lambda e: space_action)
 
-# root.bind("<Backspace>", func=backspace_action)
+root.bind("<Key>", lambda e: compare_letters(current_word) if e.char.isalpha() else None)
+
+# TODO: Need to solve backspace with one space + word color when using backspace
+root.bind("<BackSpace>", lambda e: backspace_action() if word_index > 0 and entry_text.get() == '' else None)
+
+root.bind("<space>", lambda e: space_action() if not entry_text.get().isspace() else None)
 
 root.mainloop()
-
-####
-
-# label = Label(text="This is old text")
-# label.config(text="This is new text")
-
-# button = Button(text="Click Me", command=action)
-
-# entry.insert(END, string="Some text to begin with.")
-
-# len(current_word)
